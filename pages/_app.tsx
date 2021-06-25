bd479cd8943df6e48477a02f671009f302ca50a6
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-26 03:20:58 +0700

    :sparkles: Add Ko-fi button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import useScript from "react-script-hook"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useScript({
+    src: "https://storage.ko-fi.com/cdn/scripts/overlay-widget.js",
+    onload: () => {
+      window.kofiWidgetOverlay.draw("narze", {
+        type: "floating-chat",
+        "floating-chat.donateButton.text": "สนับสนุน ฿",
+        "floating-chat.donateButton.background-color": "#f45d22",
+        "floating-chat.donateButton.text-color": "#fff",
+      })
+    },
+  })
+
   return <Component {...pageProps} />
 }
 export default MyApp
