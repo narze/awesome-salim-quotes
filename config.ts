86f57d4fb977dc827a0433f78cad8324bd714fcd
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-26 03:54:30 +0700

    :wrench: Use NEXT_PUBLIC_BASE_URL

--- config.ts
+++ config.ts
@@ -1,3 +1,1 @@
-export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : process.env.NEXT_PUBLIC_BASE_URL
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
