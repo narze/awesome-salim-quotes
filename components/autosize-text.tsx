f3058b62f2e50ca850f6c924487781de53212f1a
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-26 00:44:50 +0700

    :lipstick: Add text style

--- components/autosize-text.tsx
+++ components/autosize-text.tsx
@@ -1,5 +1,12 @@
 export default function AutosizeText({ children }: { children: string }) {
   const size = Math.min(3, children.length / 20)
   const multiplier = 4 - size
-  return <span style={{ fontSize: `${multiplier * 100}%` }}>{children}</span>
+
+  return (
+    <span style={{ fontSize: `${multiplier * 100}%` }}>
+      {"“"}
+      {children}
+      {"”"}
+    </span>
+  )
 }
