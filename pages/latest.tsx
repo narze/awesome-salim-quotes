05b1864a0aee581fe57e7c0a1987914e22ae5160
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-26 05:19:27 +0700

    :sparkles: Add latest quote shortcut

---
+++ pages/latest.tsx
@@ -0,0 +1,33 @@
+import { promises as fs } from "fs"
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
+
+import styles from "../styles/Home.module.css"
+
+export default function Latest({ entriesCount }: { entriesCount: number }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push(`/${entriesCount}`)
+  })
+
+  return (
+    <main className={styles.container}>
+      <div className={`${styles.entryRoulette}`}>Loading</div>
+    </main>
+  )
+}
+
+export async function getStaticProps() {
+  const readme = await fs.readFile("README.md", "utf8")
+
+  const entriesCount = readme
+    .split("\n")
+    .filter((line) => line.startsWith("- ")).length
+
+  return {
+    props: {
+      entriesCount,
+    },
+  }
+}
