1b86d84be4f0796b1466a206081d0ea90d326e25
Manassarn Manoonchai <manassarn@gmail.com>
2021-06-26 04:05:42 +0700

    :sparkles: Add link to old version

--- config.ts
+++ config.ts
@@ -3,5 +3,6 @@ export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 export const links = [
   { name: "Github", url: "https://github.com/narze/awesome-salim-quotes" },
   { name: "@narze", url: "https://twitter.com/narze" },
+  { name: "Old version", url: "https://narze.github.io/awesome-salim-quotes" },
   { name: "120วันเปิดประเทศ?", url: "https://thailand-grand-opening.web.app" },
 ]
