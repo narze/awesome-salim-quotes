4557354b1d394684c92071bd0fe60bac46ed9b75
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-15 00:08:21 +0700

    feat: Show API link in home page

--- config.ts
+++ config.ts
@@ -1,8 +1,9 @@
 export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
 export const links = [
+  { name: "API", url: `${baseUrl}/api` },
   { name: "Github", url: "https://github.com/narze/awesome-salim-quotes" },
-  { name: "@narze", url: "https://twitter.com/narze" },
+  { name: "By @narze", url: "https://twitter.com/narze" },
   { name: "Old version", url: "https://narze.github.io/awesome-salim-quotes" },
   { name: "120วันเปิดประเทศ?", url: "https://thailand-grand-opening.web.app" },
   { name: "ติด vs ตรวจ", url: "https://tid-vs-truad.vercel.app" },
