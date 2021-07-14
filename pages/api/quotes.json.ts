e2c94b0ed2027df8cd2f8210c12334602fc80257
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-14 22:15:38 +0700

    feat: Add all quotes API

---
+++ pages/api/quotes.json.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import { promises as fs } from "fs"
+
+type Data = {
+  quotes: {
+    id: number
+    body: string
+    url: string
+  }[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const readme = await fs.readFile("README.md", "utf8")
+
+  const entries = readme
+    .split("\n")
+    .filter((line) => line.startsWith("- "))
+    .map((l) => l.slice(2))
+
+  const quotes = entries.map((entry, index) => ({
+    id: ~~index + 1,
+    body: entries[~~index],
+    url: `https://watasalim.vercel.app/${~~index + 1}`,
+  }))
+
+  res.status(200).json({ quotes })
+}
