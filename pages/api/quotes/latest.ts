ee2a9b9ec778efb5402ce4e5411f569d8ff702b6
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-14 22:26:41 +0700

    feat: Add latest quote API

---
+++ pages/api/quotes/latest.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import { promises as fs } from "fs"
+
+type Data = {
+  quote: {
+    id: number
+    body: string
+    url: string
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const readme = await fs.readFile("README.md", "utf8")
+
+  const entries = readme
+    .split("\n")
+    .filter((line) => line.startsWith("- "))
+    .map((l) => l.slice(2))
+
+  const index = entries.length - 1
+
+  const quote = {
+    id: index + 1,
+    body: entries[index],
+    url: `https://watasalim.vercel.app/${index + 1}`,
+  }
+
+  res.status(200).json({ quote })
+}
