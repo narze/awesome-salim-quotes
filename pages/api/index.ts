54956b202234f508adb19f651758671b7e72fb7f
Manassarn Manoonchai <manassarn@gmail.com>
2022-07-02 18:08:15 +0700

    fix: add CORS

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,10 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import NextCors from "nextjs-cors"
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
   res.status(200).json({
     description: "Watasalim JSON API",
     github: "https://github.com/narze/awesome-salim-quotes",
--- pages/api/quotes/latest.ts
+++ pages/api/quotes/latest.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
+import NextCors from "nextjs-cors"
 import getEntries from "../../../utils/getEntries"
 
 type Data = {
@@ -14,6 +15,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+
   const entries = await getEntries()
 
   const index = entries.length - 1
